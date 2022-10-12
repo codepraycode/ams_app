7abdd312e454925d56cdfa6cdf973cb55e20d019
Precious Olusola <preciousolusola16@gmail.com>
2022-10-12 14:13:49 +0100

    fixed illustrations directory name

--- src/constants/assets.js
+++ src/constants/assets.js
@@ -2,8 +2,8 @@
 const placeholder = '/assets/images/icons/profile_picture.svg';
 
 
-const error500 = '/assets/images/illustration/error_500.svg';
-const error404 = '/assets/images/illustration/error_404.svg';
-const construction = '/assets/images/illustration/construction.svg';
+const error500 = '/assets/images/illustrations/error_500.svg';
+const error404 = '/assets/images/illustrations/error_404.svg';
+const construction = '/assets/images/illustrations/construction.svg';
 
 export { error500, error404, placeholder, construction };
