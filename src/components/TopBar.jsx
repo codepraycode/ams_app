fb46458b0c5ed1dfef6320987586a3a4451bf290
Precious Olusola <preciousolusola16@gmail.com>
2022-10-15 09:53:34 +0100

    Added a tiny route protection

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { placeholder } from '../constants/assets';
 import { signInUrl } from '../constants/app_urls';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAccountSignedIn, updateSignIn } from '../app/accountSlice';
 
 const TopBar = () => {
     const navigate = useNavigate();
+
+    const storeDispatch = useDispatch();
+    const signedIn = useSelector(getAccountSignedIn);
+
+    const navToAuth = ()=>{
+        if(!signedIn){
+            navigate(signInUrl);
+        }
+    }
+
+    useEffect(()=>{
+        navToAuth();
+    }, [signedIn])
 
     return (
         <div className={"top_bar"}>
@@ -13,8 +28,26 @@ const TopBar = () => {
 
 
             <div>
-                <div className="account_">
-                    <span><Link to={signInUrl}> Association </Link></span>
+                <div 
+                    className="account_" 
+                    onClick={() => {
+                        storeDispatch(updateSignIn(false));
+                    }}
+                    title="Logout"
+                >
+                    <span>
+                        <Link 
+                            to={signInUrl} 
+                            onClick={(e)=>{
+                                e.preventDefault();
+                                // storeDispatch(updateSignIn(false));
+                            }}
+                            // title="Logout"
+                        > 
+                            Association 
+                        </Link>
+                    </span>
+
                     <div className='avatar' onClick={() => navigate(signInUrl)}>
                         <img src={placeholder} alt="Logo" />
                     </div>
